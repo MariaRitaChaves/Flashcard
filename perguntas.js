1437a29b34a91f5ea8c1a11d8c8bd2f6335da676
MariaRitaChaves <141828609+MariaRitaChaves@users.noreply.github.com>
2024-10-25 08:42:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quando ocorreu a Revolução Francesa?',
+    'Em 1789'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Austrália?',
+    'Camberra'
+)
+
+criaCartao(
+    'Ciências',
+    'Quantos planetas existem no Sistema Solar?',
+    'Oito'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é aproximadamente o valor do número Pi?',
+    'Aproximadamente 3,1416'
+)
